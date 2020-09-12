2f84f1946931ba035fc77c5033da23d6791007bf
Adnan <adn201213@hotmail.com>
2020-09-12 02:56:36 -0400

    first

---
+++ src/main/java/com/adnan/springboot/demo/service/EmployeeService.java
@@ -0,0 +1,19 @@
+package com.adnan.springboot.demo.service;
+
+import java.util.List;
+
+import com.adnan.springboot.demo.entity.Employee;
+
+public interface EmployeeService {
+
+	public List<Employee> findAll();
+	
+	public Employee findById(int theId);
+	
+	public void save(Employee theEmployee);
+	
+	public void deleteById(int theId);
+
+	public List<Employee> searchBy(String theName);
+	
+}
